28b9902bed80d7d3deb6824823a267514725d1b0
Eric Walters <EricMichaelWalters@gmail.com>
2022-12-16 10:43:29 -0500

    add cart expansion functionality

--- src/features/header/header.js
+++ src/features/header/header.js
@@ -1,16 +1,28 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import styles from "./header.module.css";
-import { cartCount } from "../cart/cartSlice";
+import { cartCount, toggleVisibility } from "../cart/cartSlice";
 
 export function Header() {
   const count = useSelector(cartCount);
+  const dispatch = useDispatch();
   return (
     <header>
       <div data-testid="header--title" className={styles.title}>
         BuyMyStuff.com
       </div>
       <div className={styles.cartCount}>{count}</div>
+      {
+        //todo: refactor styles to not use float
+      }
+      <button
+        className={styles.openCart}
+        onClick={() => {
+          dispatch(toggleVisibility());
+        }}
+      >
+        Open Cart
+      </button>
     </header>
   );
 }
